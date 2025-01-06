8de6a7d1e181a6da98cafabb33c762c26f534cb4
CoderKashyap <kashyap.abhi1105@gmail.com>
2025-01-07 02:23:52 +0530

    test token

--- middleware.js
+++ middleware.js
@@ -1,55 +1,42 @@
 // export { auth as middleware } from "@/auth"
 
 
+
+
 import { NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 
 export async function middleware(request) {
   try {
-    const isProduction = true
+    console.log(request.url, "Request url");
 
-  console.log(request.url, "Request url");
+    const isAuthPage = request.nextUrl.pathname.startsWith('/login') ||
+      request.nextUrl.pathname.startsWith('/signup');
+    const isAdminPage = request.nextUrl.pathname.startsWith('/bunnx-admin');
 
-  const isAuthPage = request.nextUrl.pathname.startsWith('/login') ||
-    request.nextUrl.pathname.startsWith('/signup');
-  const isAdminPage = request.nextUrl.pathname.startsWith('/bunnx-admin');
+    const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
+    console.log("Token exists:", token, process.env.NEXTAUTH_SECRET);
 
-  const token = await getToken({
-    req: request,
-    secret: process.env.NEXTAUTH_SECRET,
-    secureCookie: isProduction,
-    cookieName: isProduction ? '__Secure-next-auth.session-token' : 'next-auth.session-token'
-  });
+    if (!token && !isAuthPage) {
+      return NextResponse.redirect(new URL('/login', request.url));
+    }
 
-  console.log("Token Debug:", {
-    hasToken: !!token,
-    hasSecret: !!process.env.NEXTAUTH_SECRET,
-    cookies: request.cookies.getAll()
-  });
-  
-  // const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
-  // console.log("Token exists:", !!token, process.env.NEXTAUTH_SECRET);
+    // Check admin role for admin pages
+    if (isAdminPage && token?.role !== 'admin' && token?.role !== 'superadmin') {
+      return NextResponse.redirect(new URL('/', request.url));
+    }
 
-  if (!token && !isAuthPage) {
+
+    if (token && isAuthPage) {
+      return NextResponse.redirect(new URL('/bunnx-admin', request.url));
+    }
+
+    return NextResponse.next();
+
+  } catch (error) {
+    console.error("Middleware error:", error);
     return NextResponse.redirect(new URL('/login', request.url));
   }
-
-  // Check admin role for admin pages
-  if (isAdminPage && token?.role !== 'admin' && token?.role !== 'superadmin') {
-    return NextResponse.redirect(new URL('/', request.url));
-  }
-
-
-  if (token && isAuthPage) {
-    return NextResponse.redirect(new URL('/bunnx-admin', request.url));
-  }
-
-  return NextResponse.next();
-
-} catch (error) {
-  console.error("Middleware error:", error);
-  return NextResponse.redirect(new URL('/login', request.url));
-}
 }
 
 export const config = {
